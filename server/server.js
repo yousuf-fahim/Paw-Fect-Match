3e713190cc8df205f415ff36a0b32c64b721ae31
Yousuf Sharif Fahim <fahim.cse.bubt@gmail.com>
2025-02-15 05:10:00 +0600

    I'm tired

--- server/server.js
+++ server/server.js
@@ -1,27 +1,22 @@
 require('dotenv').config(); // Load environment variables
 const express = require('express');
 const mongoose = require('mongoose');
-const petRouter = require('./Routes/PetRoute');
-const AdoptFormRoute = require('./Routes/AdoptFormRoute');
 const cors = require('cors');
 const path = require('path');
+
+const petRouter = require('./Routes/PetRoute');
+const AdoptFormRoute = require('./Routes/AdoptFormRoute');
 const userRouter = require('./Routes/UserRoute');
 const OtpRouter = require('./Routes/OtpRoute');
 const requireAuth = require('./Middleware/requireAuth');
 const DashboardRouter = require('./Routes/DashboardRoute');
 const AdminRouter = require('./Routes/AdminRoute'); // Import AdminRoute
-const User = require('./Model/UserModel');
-
-console.log('Environment Variables:', process.env); // Debugging line to check env variables
 
 const app = express();
 
 app.use(cors());
-
 app.use('/images', express.static(path.join(__dirname, 'images')));
-
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', OtpRouter);
@@ -30,12 +25,12 @@ app.use('/dashboard', DashboardRouter);
 app.use(requireAuth);
 app.use(petRouter);
 app.use('/form', AdoptFormRoute);
-app.use('/admin', AdminRouter); 
+app.use('/admin', AdminRouter); // Use AdminRoute
 
-mongoose.connect(process.env.mongooseURL)
+mongoose.connect(process.env.mongooseURL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('Connected to DB');
-        const PORT = 4000;
+        const PORT = process.env.PORT || 4000;
         app.listen(PORT, () => {
             console.log(`Listening on port ${PORT}`);
         });
